07cb4d62a940023aec2be7bfa0ed05a3eaa18f00
Rahul Patel <93973910+RahulPatel2727@users.noreply.github.com>
2021-12-13 19:52:34 +0530

    Create regex_basic.java

---
+++ regex_basic.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class regex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome, Please enter your email ID for the registration");
+        String str = sc.nextLine();
+        Pattern p = Pattern.compile("[a-zA-Z0-9_.]+[@](gmail|yahoo|rediff)[.]com");
+        Matcher m = p.matcher(str);
+        int ctr = 0;
+      /*here if the match is found then the index start from and ending points are printed and the group of that find pattern also printed*/
+        while(m.find()){
+            System.out.println(m.start()+ "--"+ m.end()+ "--"+m.group());
+            ctr++;
+        }
+        if(ctr==0){
+            System.out.println("Invalid Input, Please enter the valid format");
+        }
+        else {
+            System.out.println("Congratulations,You are registered successfully!");
+            System.out.println(ctr);
+        }
+
+    }
+}
